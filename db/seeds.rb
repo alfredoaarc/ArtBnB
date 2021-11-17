ad0dbd7dd329f8931b606a70f24e3d7e154e67a1
Sara Rahali <rahalisara@hotmail.fr>
2021-11-17 14:20:18 +0100

    page du details modifiee

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ photos_urls.each do |photo|
 
   artwork = Artwork.create(
   title: Faker::Books::CultureSeries.book,
-  description: 'Lorem ipsum',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   location: locations.sample,
   price: Faker::Commerce.price(range: 90..500, as_string: true),
   category: categories.sample,
