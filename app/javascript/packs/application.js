c7c96bb9a1f39c740fbb1b828a54df41ce4b1705
Sara Rahali <rahalisara@hotmail.fr>
2021-11-18 23:41:10 +0100

    change of the print

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,16 +24,15 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { initFlatpickr } from "../plugins/flatpickr";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
+
 });
 
 // app/javascript/packs/application.js
-import { initFlatpickr } from "../plugins/flatpickr";
-
-initFlatpickr();
