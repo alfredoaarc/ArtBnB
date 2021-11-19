b163c68cd5762a29f1bbb9c5f5c08d2e42c3b43b
Sara Rahali <rahalisara@hotmail.fr>
2021-11-19 16:04:43 +0100

    correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :artworks, dependent: :destroy
-  has_many :rentals, dependent: :destroy
+  has_many :rentals, through: :artworks
 end
